48f6fb2be6e7133d3528ac13de6b0e362d873882
mekchandr <35653274+mekchandr@users.noreply.github.com>
2018-02-09 15:58:16 +0530

    Create array.c

---
+++ array.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,k[100],i,y=0;
+    printf("enter the value of b:");
+    scanf("%d",&b);
+    printf("enter an array:");
+    for(i=0;i<a;i++)
+    {
+        scanf("%d",&k[i]);
+    }
+    for(i=0;i<b;i++)
+    {
+        y+=k[i];
+    }
+    printf("%d",y);
+        return 0;
+}
+  printf("enter the value for a :");
+    scanf("%d",&a);
+  
+
